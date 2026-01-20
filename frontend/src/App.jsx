da1014aef635c09d5956f89c7ab60cad264a870a
tuantran11swd <tuantran11.swd@gmail.com>
2026-01-20 14:38:09 +0700

    feat(router): add routing and create basic pages and layout components
    - Implement React Router in App with routes for Home, Doctors, Contact, About, Login, MyProfile, MyAppointments, and Appointment with parameter
    - Wrap App component in BrowserRouter in main.jsx for routing context
    - Create basic functional components for pages: About, Appointment, Contact, Doctors, Home, Login, MyAppointments, MyProfile
    - Add simple Navbar and Footer components as placeholders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Appointment from "./pages/Appointment";
+import Contact from "./pages/Contact";
+import Doctors from "./pages/Doctors";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyAppointments from "./pages/MyAppointments";
+import MyProfile from "./pages/MyProfile";
+
 function App() {
   return (
-    <div>
-      <h1 className="font-bold text-3xl underline">Hello world!</h1>
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Doctors />} path="/doctors" />
+        <Route element={<Contact />} path="/contact" />
+        <Route element={<About />} path="/about" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<MyProfile />} path="/my-profile" />
+        <Route element={<MyAppointments />} path="/my-appointments" />
+        <Route element={<Appointment />} path="/appointment/:docId" />
+      </Routes>
+      <Footer />
     </div>
   );
 }
